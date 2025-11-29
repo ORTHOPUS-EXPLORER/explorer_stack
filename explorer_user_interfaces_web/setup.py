e3ef038bdc2cfe2443f12acfabdfb0ca6e6a67cd
Thomas Solatges <thomas.solatges@orthopus.com>
2025-11-29 09:54:05 +0100

    clean: remove unnecessary files

--- explorer_user_interfaces_web/setup.py
+++ explorer_user_interfaces_web/setup.py
@@ -51,7 +51,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'requirements.txt']),
+        ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', [
             'launch/web_gui.launch.py',
             'launch/explorer_with_web_gui.launch.py'
