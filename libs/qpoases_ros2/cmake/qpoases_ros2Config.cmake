44c8bed2d7d45da605265a70abad2b2690cce284
Corentin Ducatez <corentin.ducatez@orthopus.com>
2026-05-18 13:11:32 +0000

    refactor: fix qpoases_ros2 CMake

--- libs/qpoases_ros2/cmake/qpoases_ros2Config.cmake
+++
@@ -1,8 +0,0 @@
-set(qpoases_ros2_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include")
-set(qpoases_ros2_LIBRARIES "${CMAKE_INSTALL_PREFIX}/lib/libqpoases_ros2.a")
-
-add_library(qpoases_ros2 STATIC IMPORTED)
-set_target_properties(qpoases_ros2 PROPERTIES
-  IMPORTED_LOCATION "${CMAKE_INSTALL_PREFIX}/lib/libqpoases_ros2.a"
-  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include"
-)
